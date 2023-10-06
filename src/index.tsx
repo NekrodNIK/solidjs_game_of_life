b06e335cffe47031204b3565f924ea664b5181aa
NekrodNIK <60639354+NekrodNIK@users.noreply.github.com>
2023-10-06 15:37:52 +0500

    add Rubik font + background change

--- src/index.tsx
+++ src/index.tsx
@@ -3,11 +3,11 @@ import "./index.css"
 import { createSignal } from 'solid-js';
 
 function App() {
-  const [getCount, setCount] = createSignal(0.0)
-
-  setInterval(() => setCount((getCount()+0.1)), 100);
-
-  return <h1>Game Of Life, seconds: {getCount().toFixed(1)}</h1>
+  return (
+    <>
+      <h1 id="game_title">Game Of Life</h1>
+    </>
+  )
 }
 
 render(App, document.getElementById("root")!)
