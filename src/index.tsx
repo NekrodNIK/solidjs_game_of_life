f086caa5fa0dbf9bd3c8e4c8e10d15c2b00f4794
NekrodNIK <60639354+NekrodNIK@users.noreply.github.com>
2023-10-07 13:24:01 +0500

    minor changes

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,12 @@
 import { render } from 'solid-js/web';
-import "./index.css"
-import { createSignal } from 'solid-js';
+import "./index.css";
+
 
 function App() {
   return (
     <>
       <h1 id="game_title">Game Of Life</h1>
+      <div id="game_field"></div>
     </>
   )
 }
